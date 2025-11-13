d53991272ad762744a4200c3b0c9f5d2dcf0eac7
Lebogang <lbtsatsi18@gmail.com>
2025-11-13 12:47:22 +0200

    WishList Page High fedility UI/UX

--- lib/wishlist-feature/presentation/wishlist_page.dart
+++ lib/wishlist-feature/presentation/wishlist_page.dart
@@ -17,61 +17,98 @@ class _WishListPageState extends ConsumerState<WishListPage> {
   Widget build(BuildContext context) {
     final wishProducts = ref.watch(wishProvider);
     return Scaffold(
-      appBar: AppBar(title: Text('Wish List'), centerTitle: true),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: const Color(0xFF1B1A1F),
+        title: Text('Wish List', style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
       body: wishProducts.isEmpty
-          ? Center(child: Text('Wish list is empty'))
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(CupertinoIcons.heart, size: 72),
+                  SizedBox(height: 10),
+                  Text('Wish list is empty', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+            )
           : Expanded(
-              child: ListView.builder(
-                itemCount: wishProducts.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Image.asset(
-                      width: 60,
-                      wishProducts[index].image,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Image.asset(
-                          width: 60,
-                          'assets/images/fallback.jpg',
-                          fit: BoxFit.cover,
-                        );
-                      },
-                    ),
-                    title: Text(wishProducts[index].name),
-                    subtitle: Text(
-                      'R ${wishProducts[index].price.toStringAsFixed(2)}',
-                    ),
-                    trailing: Container(
-                      decoration: BoxDecoration(color: const Color(0x1A000000)),
-                      width: 100,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              ref
-                                  .read(wishProvider.notifier)
-                                  .removeProduct(wishProducts[index]);
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: ListView.builder(
+                  itemCount: wishProducts.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 5,
+                      ),
+                      child: Card(
+                        color: Colors.white,
+                        child: ListTile(
+                          leading: Image.asset(
+                            width: 60,
+                            wishProducts[index].image,
+                            errorBuilder: (context, error, stackTrace) {
+                              return Image.asset(
+                                width: 60,
+                                'assets/images/fallback.jpg',
+                                fit: BoxFit.cover,
+                              );
                             },
-                            icon: Icon(CupertinoIcons.trash),
                           ),
-                          IconButton(
-                            onPressed: !wishProducts[index].inStock
-                                ? null
-                                : () {
-                                    ref
-                                        .read(cartProvider.notifier)
-                                        .addProduct(wishProducts[index]);
+                          title: Text(wishProducts[index].name),
+                          subtitle: Text(
+                            'R ${wishProducts[index].price.toStringAsFixed(2)}',
+                          ),
+                          trailing: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            width: 100,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                IconButton(
+                                  onPressed: () {
                                     ref
                                         .read(wishProvider.notifier)
                                         .removeProduct(wishProducts[index]);
                                   },
-                            icon: Icon(CupertinoIcons.bag),
+                                  icon: Icon(
+                                    Icons.delete_outline,
+                                    color: const Color(0xFF1B1A1F),
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: !wishProducts[index].inStock
+                                      ? null
+                                      : () {
+                                          ref
+                                              .read(cartProvider.notifier)
+                                              .addProduct(wishProducts[index]);
+                                          ref
+                                              .read(wishProvider.notifier)
+                                              .removeProduct(
+                                                wishProducts[index],
+                                              );
+                                        },
+                                  icon: Icon(
+                                    CupertinoIcons.bag_badge_plus,
+                                    color: const Color(0xFF1B1A1F),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
       bottomNavigationBar: CBottomNavigation(),
