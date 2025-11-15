e09a67ef9364f79d53c5bf196b2c3312c2ddd90f
Lebogang <lbtsatsi18@gmail.com>
2025-11-15 15:12:33 +0200

    Firebase Flutter Hookup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:e_commerce/app.dart';
+import 'package:e_commerce/firebase_options.dart' show DefaultFirebaseOptions;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: App()));
 }
